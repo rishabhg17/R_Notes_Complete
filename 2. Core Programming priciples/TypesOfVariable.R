791e61d682da03eb4e670d0a7e5d409888d21c43
rishabhg17 <g.rishabh13@gmail.com>
2021-01-05 23:28:12 +0530

    Core Principles

---
+++ 2. Core Programming priciples/TypesOfVariable.R
@@ -0,0 +1,26 @@
+
+
+
+#integer
+x<-2L
+typeof(x)
+
+#double
+y<-2.5
+typeof(y)
+
+#if integer without L it will show type as double
+z<-2
+typeof(z)
+
+#complex number 
+a<- 3 + 2i
+typeof(a)
+
+#character
+b<- "h"
+typeof(b)
+
+#logical
+q1<- T
+typeof(q1)
