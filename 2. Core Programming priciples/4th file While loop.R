623afe78f1dcd3630b64bb2011ec946435029a3e
rishabhg17 <g.rishabh13@gmail.com>
2021-01-06 15:17:24 +0530

    Core Principles

---
+++ 2. Core Programming priciples/4th file While loop.R
@@ -0,0 +1,16 @@
+
+
+
+while(TRUE){
+  print("Hello")
+}
+
+#this will print hello infinitely
+
+counter<-1
+while(counter<12){
+  print(counter)
+  counter<-counter+1
+}
+
+#this will execute according to the condition we provided
