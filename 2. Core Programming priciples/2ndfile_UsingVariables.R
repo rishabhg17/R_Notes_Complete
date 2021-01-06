6347de5d37fe6e44e3d074a86b53b07f9038fea2
rishabhg17 <g.rishabh13@gmail.com>
2021-01-06 13:59:20 +0530

    Core Principles

---
+++ 2. Core Programming priciples/2ndfile_UsingVariables.R
@@ -0,0 +1,32 @@
+#we will now learn how to use variables 
+
+A<- 10
+B<- 5
+
+C<- 10+5
+# but to print the value of C we simply need to write 
+C
+
+#Note- In R we have to selected all the code we wanna run or run each line sperately becoz in R only the line with cursur is run at time of execution 
+
+#lets take one more example
+
+#variable1
+var1<-2.5
+#variable2
+var2<-4
+
+result<-var1/var2
+result
+
+answer<-sqrt(var2)
+answer
+
+#sqrt is a function which gives the square root
+
+#lets now work with characters
+
+greeting<- "Hello"
+name<- "Rishabh"
+message<- paste(greeting, name)
+message
