56c61c221bbd7b72f018795ac953976f73336105
rishabhg17 <g.rishabh13@gmail.com>
2021-01-09 20:23:17 +0530

    Dataframes

---
+++ 5. DataFrames/using dollar sign.R
@@ -0,0 +1,12 @@
+stat
+head(stat)
+stat[3,3]
+stat[3,"Birth.rate"]
+
+# ----- $ sign using like below give whole column 
+
+stat$Internet.users
+stat$Internet.users[2]
+
+
+levels(stat$Income.Group)
